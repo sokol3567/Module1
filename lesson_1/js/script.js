0e30a3d30551c57686965be0adcfa50d69481da5
Sokoshvili <sia_06.11@mail.ru>
2022-07-04 19:25:10 +0300

    Добавление второго вида товара

--- lesson_1/js/script.js
+++ lesson_1/js/script.js
@@ -1,5 +1,5 @@
 'use strict'
-
+{
 const nameProduct = 'Laptop';
 const countProduct = 15;
 const categoryProduct = 'Technology';
@@ -7,3 +7,14 @@ const priceProduct = 50000;
 
 console.log('Наименование товара: ' + nameProduct);
 console.log('Общая сумма товара: ' + (countProduct * priceProduct) );
+}
+
+{
+    const nameProduct = 'Phone';
+    const countProduct = 31;
+    const categoryProduct = 'Technology';
+    const priceProduct = 25000;
+    
+    console.log('Наименование товара: ' + nameProduct);
+    console.log('Общая сумма товара: ' + (countProduct * priceProduct) );  
+}
